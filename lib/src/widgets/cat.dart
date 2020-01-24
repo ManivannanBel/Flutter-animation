08a81a38fb2f40844cca0709677d745c8b6f44d4
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2020-01-24 23:09:36 +0530

    cat animator

--- lib/src/widgets/cat.dart
+++ lib/src/widgets/cat.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Cat extends StatelessWidget{
   Widget build(context){
     return Image.network(
-      'https://i.imgur.com/QwhZRyl.png'
+      'https://www.freeiconspng.com/uploads/black-cat-icon-1.png'
     );
   }
 }
